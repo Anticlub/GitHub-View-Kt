6906da47387a26b5dbc2529c989bf13d3f8e94c1
Anticlub <cristofdzfdz@gmail.com>
2026-03-18 16:49:53 +0100

    Added ReposActivity and layout with image section and RecicleView

---
+++ app/src/main/java/com/example/github_view_kt/ui/ReposActivity.kt
@@ -0,0 +1,21 @@
+package com.example.github_view_kt.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.github_view_kt.R
+
+class ReposActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_repos)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
