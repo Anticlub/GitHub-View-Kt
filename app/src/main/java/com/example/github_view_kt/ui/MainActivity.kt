9cd09c6a110e716e59757d8ad9efa499ecdd3654
Anticlub <cristofdzfdz@gmail.com>
2026-03-18 16:24:56 +0100

    Moved MainActivity to ui package and update layout xml

--- app/src/main/java/com/example/github_view_kt/ui/MainActivity.kt
+++ app/src/main/java/com/example/github_view_kt/ui/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.example.github_view_kt
+package com.example.github_view_kt.ui
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.github_view_kt.R
 
+private lateinit var btnSearch: Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +20,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnSearch = findViewById(R.id.btnSearchMain)
+
+        btnSearch.setOnClickListener {
+            val username = findViewById<EditText>(R.id.etUsername).text.toString().trim()
+            if(username.isNotEmpty()){
+                Toast.makeText(this, "Usuario: $username", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Introduce un usario", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
